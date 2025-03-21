aa2a29de79930fac666e479e0fd4f888c00c0927
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-03-21 08:15:22 -0500

    Update ftp_upload.sh

--- mtr/scripts/ftp_upload.sh
+++ mtr/scripts/ftp_upload.sh
@@ -1,12 +1,9 @@
 # Script to upload mtr logs to an ftp server to view the logs and download the logs
 
-# Needed dependencies: ftp-upload (ubuntu/debian package)
-
-sudo apt install ftp-upload
+# Needed dependencies: ftp-upload (ubuntu/debian package - this installs in the staging script)
 
 # Remove the server, user, and password fields and populate them with the correct information
 
 cd /mtr/logs/archive
 
 ftp-upload -h {SERVER} -u {USER} --password {PASSWORD} *.txt
-
