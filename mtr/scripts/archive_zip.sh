abe1a9bd8209cf90084274a850a2df5bc949f9c6
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-03-21 12:58:04 -0500

    Update archive_zip.sh

--- mtr/scripts/archive_zip.sh
+++ mtr/scripts/archive_zip.sh
@@ -1,5 +1,5 @@
 # Zipping current date directory for ftp transfer...
-zip -r ""`date +"%m-%d-%Y"`.zip ""`date +"%m-%d-%Y"`
+zip -r /mtr/logs/archive/""`date +"%m-%d-%Y"`.zip /mtr/logs/archive/""`date +"%m-%d-%Y"`
 
 # First date command is for grabbing the current date and inserting it into the name of the zip file
 
